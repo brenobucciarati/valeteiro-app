3c5ab394a24e478dddf191d1459ffc4093e30317
Breno Vinicius <126256590+brenobucciarati@users.noreply.github.com>
2025-08-07 13:57:11 -0300

    Update auto_migrate.py

--- auto_migrate.py
+++ auto_migrate.py
@@ -2,19 +2,17 @@ from app import app, db
 from models import User
 from flask_migrate import upgrade
 
+print("🔁 Aplicando migrações...")
 with app.app_context():
-    print("🔁 Aplicando migrações...")
-    print("📦 Usando banco de dados:", app.config["SQLALCHEMY_DATABASE_URI"])  # <-- DEBUG CRUCIAL
-
     upgrade()
     print("✅ Migrações aplicadas com sucesso.")
 
-    existing_user = User.query.filter_by(username="admin").first()
-    if not existing_user:
+    # Criar usuário admin se não existir
+    if not User.query.filter_by(username="admin").first():
         user = User(username="admin")
         user.set_password("admin@2025")
         db.session.add(user)
         db.session.commit()
-        print("✅ Usuário admin criado.")
+        print("✅ Usuário admin criado com sucesso.")
     else:
         print("ℹ️ Usuário admin já existe.")
