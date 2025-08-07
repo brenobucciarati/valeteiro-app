ec496a5bba65c98987d108f09cdedf9c7bf22534
Breno Vinicius <126256590+brenobucciarati@users.noreply.github.com>
2025-08-07 09:22:20 -0300

    Update criar_usuario.py

--- criar_usuario.py
+++ criar_usuario.py
@@ -3,7 +3,8 @@ from models import db, User
 
 with app.app_context():
     novo = User(username="admin")
-    novo.set_password("admin@2025")  # 🔐 Escolha uma senha forte
+    novo.set_password("admin@2026")  # 🔐 Escolha uma senha forte
     db.session.add(novo)
     db.session.commit()
     print("Usuário criado com sucesso.")
+
