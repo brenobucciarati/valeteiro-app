cffb52c58c1d87f5753f29f0214212d3b5c40acc
Breno Vinicius <126256590+brenobucciarati@users.noreply.github.com>
2025-08-07 09:50:33 -0300

    Update models.py

--- models.py
+++ models.py
@@ -27,15 +27,19 @@ class Programacao(db.Model):
 
     veiculo = db.relationship("Veiculo", backref="programacoes")
 
-class User(db.Model, UserMixin):
-    __tablename__ = "users"
+class User(db.Model):
+    __tablename__ = "users"  # <- Adicione esta linha
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, nullable=False)
-    password_hash = db.Column(db.String(512), nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
 
