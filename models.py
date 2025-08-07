7b8d57294654027ed4bff595a9ed59d7a7b08a73
Breno Vinicius <126256590+brenobucciarati@users.noreply.github.com>
2025-08-07 14:56:47 -0300

    Update models.py

--- models.py
+++ models.py
@@ -31,7 +31,7 @@ class User(db.Model, UserMixin):
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, nullable=False)
-    password_hash = db.Column(db.String(512), nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
