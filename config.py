c82bb9155eba02d2e584d2e151ed810adc1daefb
Breno Vinicius <126256590+brenobucciarati@users.noreply.github.com>
2025-08-07 08:21:46 -0300

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,12 @@ import os
 
 class Config:
     BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'inspecaovaleteiro_frota.db')
+    
+    # Usa PostgreSQL no Render ou SQLite localmente
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        'DATABASE_URL',
+        'sqlite:///' + os.path.join(BASE_DIR, 'inspecaovaleteiro_frota.db')
+    )
+    
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv('SECRET_KEY', 'minhasecret')
